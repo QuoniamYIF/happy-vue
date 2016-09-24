7fc671a2675490ac087c0f2d5571b10ee40e0fb1
QuoniamYIF <quoniamy@gmail.com>
2016-09-24 15:08:21 +0800

    add dev branch

---
+++ fetchmyGithuCommit/app.js
@@ -0,0 +1,36 @@
+var apiUrl = "https://api.github.com/repos/QuoniamYIF/myVueStudy/commits?per_page=3&sha=";
+
+var demo = new Vue({
+	el: '#demo',
+
+	data: {
+		branches: ['master', 'dev'],
+		currentBranch: 'master',
+		commits: null
+	},
+
+	created: function () {
+    this.fetchData()
+  },
+
+	watch: {
+	    currentBranch: 'fetchData'
+  },
+
+	filters: {
+    truncate: function (v) {
+      var newline = v.indexOf('\n')
+      return newline > 0 ? v.slice(0, newline) : v
+    },
+    formatDate: function (v) {
+      return v.replace(/T|Z/g, ' ')
+    }
+	},
+
+	methods: {
+		fetchData: function() {
+			var self = this;
+			$.get()
+		}
+	}
+});
