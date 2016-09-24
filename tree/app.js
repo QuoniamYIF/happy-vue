d92b57ec858282e536aa833950a5988b8003f950
QuoniamYIF <quoniamy@gmail.com>
2016-09-24 17:56:31 +0800

    add table tree dir

---
+++ tree/app.js
@@ -0,0 +1,51 @@
+var data = {
+  name: 'My Tree',
+  children: [
+    { name: 'hello' },
+    { name: 'wat' },
+    {
+      name: 'child folder',
+      children: [
+        {
+          name: 'child folder',
+          children: [
+            { name: 'hello' },
+            { name: 'wat' }
+          ]
+        },
+        { name: 'hello' },
+        { name: 'wat' },
+        {
+          name: 'child folder',
+          children: [
+            { name: 'hello' },
+            { name: 'wat' }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+Vue.component('item', {
+  template: "#item-template", 
+  props: {
+    model: Object
+  },
+  data: function() {
+
+  },
+  computed: {
+
+  },
+  methods: {
+    
+  }
+});
+
+var demo = new Vue({
+  el: '#demo',
+  data: {
+    treeData: data
+  }
+})
