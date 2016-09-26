9fbd07a4778877bff5b340baa73d4c5719f8dc9d
QuoniamYIF <quoniamy@gmail.com>
2016-09-26 14:08:36 +0800

    add something else

--- tree/app.js
+++ tree/app.js
@@ -2,28 +2,7 @@ var data = {
   name: 'My Tree',
   children: [
     { name: 'hello' },
-    { name: 'wat' },
-    {
-      name: 'child folder',
-      children: [
-        {
-          name: 'child folder',
-          children: [
-            { name: 'hello' },
-            { name: 'wat' }
-          ]
-        },
-        { name: 'hello' },
-        { name: 'wat' },
-        {
-          name: 'child folder',
-          children: [
-            { name: 'hello' },
-            { name: 'wat' }
-          ]
-        }
-      ]
-    }
+    { name: 'wat' }
   ]
 }
 
@@ -33,13 +12,31 @@ Vue.component('item', {
     model: Object
   },
   data: function() {
-
+    return {
+      open: false
+    }
   },
   computed: {
-
+    isFolder: function() {
+      return this.model.children && this.model.children.length
+    }
   },
   methods: {
-    
+    toggle: function() {
+      if(this.isFolder) {
+        this.open = !this.open
+      }
+    },
+    changeType: function() {
+      Vue.set(this.model, 'children', [])
+      this.addChild()
+      this.open = true
+    },
+    addChild: function() {
+      this.model.children.push({
+        name: 'new stuff'
+      })
+    }
   }
 });
 
