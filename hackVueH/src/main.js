57941563238b7650efd97b92dd023e6f2cd91963
QuoniamYIF <quoniamy@gmail.com>
2016-09-29 13:12:17 +0800

    hackVueH init

---
+++ hackVueH/src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { domain, fromNow } from './filters'
+import App from './components/App.vue'
+import NewsView from './components/NewsView.vue'
+import ItemView from './components/ItemView.vue'
+import UserView from './components/UserView.vue'
