3608be3be14e127f733c27cbed5805851afa3573
QuoniamYIF <quoniamy@gmail.com>
2016-10-11 17:28:07 +0800

    2016/10/11

---
+++ hackVueH2/src/router/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+import { createListView } from '../views/CreateListView'
+import ItemView from '../views/ItemView.vue'
+import UserView from '../views/UserView.vue'
+
+export default new Router({
+    
+})
