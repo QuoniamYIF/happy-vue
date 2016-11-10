4da9dcf6b61f7020aed4c2fd68ed3949b02c9235
QuoniamYIF <quoniamy@gmail.com>
2016-11-10 16:44:23 +0800

    a little end

---
+++ nodevb/dao/Mapping.js
@@ -0,0 +1,46 @@
+
+var rskParaMapping = {
+	// insert:'INSERT INTO user(id, name, age) VALUES(0,?,?)',
+	// update:'update user set name=?, age=? where id=?',
+	// delete: 'delete from user where id=?',
+	// queryById: 'select * from user where id=?',
+	queryAll: 'select * from rsParamVarDef',
+	update: 'update '
+};
+
+var evntCnfigMapping = {
+	// insert:'INSERT INTO user(id, name, age) VALUES(0,?,?)',
+	// update:'update user set name=?, age=? where id=?',
+	// delete: 'delete from user where id=?',
+	// queryById: 'select * from user where id=?',
+	queryAll: 'select * from evntCnfig'
+};
+
+var ruleCmpRstMapping = {
+	queryAll: 'select * from ruleCmpRst',
+	queryById: 'select * from ruleCmpRst where ruleRstId=?'
+}
+//规则计算结果语句表
+var ruleCmpRstSqlMapping = {
+	queryAll: 'select * from ruleCmpRstSql',
+	queryById: 'select * from ruleCmpRstSql where cmpDte=? and ruleSetId=? and ruleId=? and cmpBatNo=?'
+}
+//规则计算结果明细表
+var ruleCmpRstTraceMapping = {
+	queryAll: 'select * from ruleCmpRstTrace',
+	queryById: 'select * from ruleCmpRstTrace where cmpDte=? and ruleSetId=? and ruleId=? and cmpBatNo=? and ruleBelongType=? and rstBelongTo=?'
+}
+//规则计算结果明细语句
+var ruleCmpRstTrcSqlMapping = {
+	queryAll: 'select * from ruleCmpRstTrcSql',
+	queryById: 'select * from ruleCmpRstTrcSql where cmpDte=? and ruleSetId=? and ruleId=? and cmpBatNo=?'
+}
+ 
+module.exports = {
+    "rskPara": rskParaMapping,
+    "evntCnfig": evntCnfigMapping,
+	"rleCRst": ruleCmpRstMapping,
+	"rleCRSql": ruleCmpRstSqlMapping,
+	"rleCRT": ruleCmpRstTraceMapping,
+	"rleCRTSql": ruleCmpRstTrcSqlMapping
+};
